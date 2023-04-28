9d18c31dca6f87c9e55bf448c76cf53f0c18bd9e
keerthana-Muthukumarasamy <92450850+keerthana-Muthukumarasamy@users.noreply.github.com>
2023-04-28 15:52:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from './components/homepage';
 import form from './components/form';
 import Gform from './components/form';
+import  "./components/edit.css"
 const name="keerthana"
 const department="cse"
 function App() {
--- form.jsx
+++ form.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 function Gform() {
     return (
-        <div classname="form-main" >
+        <div className="form-main" >
         <form>
              <div>
-            <label> enter the name</label>
+            <label1 className="l1"> name </label1>
             <input type="text" placeholder="enter your name" name="name" />
           </div>
           <div>
-            <label> enter the rollo</label>
-            <input type="text" placeholder="enter your rollno" name="name" />
+            <label2 className="l2">rollno</label2>
+            <input type="text" placeholder="enter your rollno" name="name" className="l1"/>
+          </div>
+          <div>
+            <label3  className="l3">department</label3>
+            <input type="text" placeholder="enter your department" name="name" />
+          </div>
+          <div>
+            <label4  className="l4">gender</label4>
+            <input type="radio" value="Male" name="gender" /> Male
+        <input type="radio" value="Female" name="gender" /> Female
+        <input type="radio" value="Other" name="gender" /> Other
+          </div>
+          <div>
+            <label5  className="l5"> enter the phonenumber</label5>
+            <input type="text" placeholder="enter your phone number" name="name" />
+          </div>
+          <div>
+            <label6  className="l6"> enter your emailid</label6>
+            <input type="text" placeholder="enter your email" name="name" />
           </div>
           <div>
             <button type ="">Submit</button>
